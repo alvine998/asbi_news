ce70bfb33c600c6b9292e571fe4d46fab1e7e813
alvin <alvinecom2018@gmail.com>
2025-01-10 15:25:42 +0700

    --

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto text-center">
-        <p className="text-center">Powered By Darma Buana Indonesia</p>
+        <p className="text-center">Powered By PT Aksi Senyum Bersama Indonesia</p>
         <p>&copy; 2025 ASBI News. All rights reserved.</p>
       </div>
     </footer>
